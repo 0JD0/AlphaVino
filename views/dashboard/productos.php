4821a5ec580696dd539e1a8a443f40a9ee8f33ed
0JD0 <eusojdivad0@gmail.com>
2019-04-22 19:44:25 -0600

    2

--- views/dashboard/productos.php
+++ views/dashboard/productos.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>productos</title>
+    <title>Productoss</title>
     <link type="image/png" rel="icon" href="../../resources/img/logo.png" />
     <link type="text/css" rel="stylesheet" href="../../resources/css/materialize.css">
     <link type="text/css" rel="stylesheet" href="../../resources/css/icons.css" />
